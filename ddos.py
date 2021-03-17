4c92b370e554951cd8081d87b0e06853f313e524
Rexxxxxxxxxxxxxxx <80580425+Rexxxxxxxxxxxxxxx@users.noreply.github.com>
2021-03-17 15:48:45 +0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -20,13 +20,12 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : Rex"
+print "You Tube : https://www.youtube.com/channel/UCJP_r9Ymb7UQZBWK8-OSVmA"
+print "github   : https://github.com/Rexxxxxxxxxxxxxxx"
 print
 ip = raw_input("IP Target : ")
-port = input("Port       : ")
+port = input("Port        : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
